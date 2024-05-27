834101f667f470006108383e43b1fa05c707125b
lua-rita <106076924+lua-rita@users.noreply.github.com>
2024-05-27 12:16:13 -0300

    a

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const dataAtual = new Date();
+const dataAtual = new Date ();
 let dataObjetivo = prompt("Data final ANO-MES-DIA Exemplo: 2024-04-10");
 
 dataObjetivo = new Date(dataObjetivo + "T23:59:59");
